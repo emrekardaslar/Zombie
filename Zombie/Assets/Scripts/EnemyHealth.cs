1695dd8ce6271f8e2095e5afec2299ccdb295afc
Emre Kardaşlar <emre.kardaslar@gmail.com>
2021-04-07 20:59:41 +0300

    Zombie Added

--- Zombie/Assets/Scripts/EnemyHealth.cs
+++ Zombie/Assets/Scripts/EnemyHealth.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float hp = 100f;
-   
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
     public void TakeDamage(float damage)
     {
         BroadcastMessage("OnDamageTaken");
         hp -= damage;
         if (hp <=0)
         {
-            Destroy(gameObject);
+            Die();
+            //Destroy(gameObject);
         }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die");
     }
 }
