c6792acc70518e5dc21cc66eb6707e10b22777ea
Emre Kardaşlar <emre.kardaslar@gmail.com>
2021-04-09 09:18:33 +0300

    Pause Menu

---
+++ Zombie/Assets/Scripts/GoldPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPickup : MonoBehaviour
+{
+    [SerializeField] Canvas gameWonCanvas;
+    private void Start()
+    {
+        gameWonCanvas.enabled = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameWonCanvas.enabled = true;
+            Time.timeScale = 0;
+            FindObjectOfType<WeaponSwitcher>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
---
+++ Zombie/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    [SerializeField] Canvas pauseMenu;
+    [SerializeField] GameObject player;
+
+    private void Start()
+    {
+        pauseMenu.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        gameIsPaused = true;
+        Time.timeScale = 0;
+        pauseMenu.enabled = true;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
+        FindObjectOfType<Weapon>().enabled = false;
+        player.GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>().enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        
+    }
+
+    public void Resume()
+    {
+        gameIsPaused = false;
+        Time.timeScale = 1;
+        pauseMenu.enabled = false;
+        FindObjectOfType<WeaponSwitcher>().enabled = true;
+        FindObjectOfType<Weapon>().enabled = true;
+        player.GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>().enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        
+    }
+}
